dfbbf17b6b196b589090adbc447a86fc3bf1bff4
mickaelzhang <mickael.zhg@gmail.com>
2016-04-11 18:40:49 +0200

    Add session_start()
    Temporary solution, I add directly in $_SESSION
    Replace with FB Connect ASAP

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+session_start();
 
 // Config
 include 'config/options.php';
 include 'config/database.php';
+
+// Force a SESSION right now, because FB connect don't work, to delete when FB Connect work
+// $_SESSION['user_id']    = 1;
+// $_SESSION['first_name']  = 'Mickael';
 
 // Get the query
 $q = empty($_GET['q']) ? '' : $_GET['q'];
@@ -16,8 +21,10 @@ else if($q == 'login-callback')
 	$page = 'login-callback';
 else if($q == 'explore')
 	$page = 'explore';
-else if($q == 'seance')
+else if(preg_match('/^seance\/[0-9]+$/',$q))
 	$page = 'seance';
+// else if($q == 'seance')
+// 	$page = 'seance';
 else if($q == 'dashboard')
 	$page = 'dashboard';
 else if($q == 'creation')
@@ -32,3 +39,8 @@ include 'views/partials/nav.php';
 include 'views/pages/'.$page.'.php';
 include 'views/partials/footer.php';
 include 'views/partials/html-bottom.php';
+
+echo '<h1>DEBUG</h1>';
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
