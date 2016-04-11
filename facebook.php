af9ba6b2a7b6c1fb1eb84e74e0fe6719b56e4305
Nans Thomas <nans.thomas@hetic.net>
2016-04-11 15:41:16 +0200

    First Commit

---
+++ facebook.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'vendor/facebook/autoload.php';
+
+$appId = '1703544296582819';
+$appSecret = '841701dd4b9b535e585993653976e086';
+
+$helper = new FacebookRedirectLoginHelper('http://localhost:8888/login.php');
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+// Config
+include 'config/options.php';
+include 'config/facebook.php';
+// include 'config/database.php';
+
+// Get the query
+$q = empty($_GET['q']) ? '' : $_GET['q'];
+
+// Routes
+if($q == '')
+	$page = 'home';
+else if($q == 'about')
+	$page = 'about';
+else if($q == 'news')
+	$page = 'news';
+else if(preg_match('/^news\/[-a-z0-9]+$/',$q)) // news/mon-titre-d-actualite
+	$page = 'news-single';
+else
+	$page = '404';
+
+// Includes
+include 'controllers/'.$page.'.php';
+include 'views/partials/header.php';
+include 'views/pages/'.$page.'.php';
+include 'views/partials/footer.php';
