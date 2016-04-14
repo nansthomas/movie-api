894ad53ca16433ea1d8fb0ac1940ab456f13325d
mickaelzhang <mickael.zhg@gmail.com>
2016-04-14 02:13:14 +0200

    Change variable on the create page

--- views/pages/create.php
+++ views/pages/create.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<form action="#" method="post">
 			<section>
-				<h3><input type="text" name="event_name" id="event_name" value="<?= $event_name == '' ? 'Nom de lévènement' : $event_name ?>" ></h3>
+				<h3><input type="text" name="event_name" id="event_name" value="<?= $form_data->event_name == '' ? 'Nom de lévènement' : $form_data->event_name ?>" ></h3>
 			</section>
 			<section>
 
@@ -12,16 +12,16 @@
 				<h4>INFORMATIONS GENERALES</h4>
 				<div>
 					<label for="begin_date">Date</label>
-					<input type="date" name="begin_date" id="begin_date" min="<?= $today ?>" value="<?= $begin_date != '' ? $begin_date : $today ?>">
+					<input type="date" name="begin_date" id="begin_date" value="<?= $form_data->begin_date != '' ? $form_data->begin_date : $today ?>">
 				</div>
 
 				<div>
 					<label for="begin_hour">Heure</label>
-					<input type="time" name="begin_hour" id="begin_hour" min="<?= $time ?>" value="<?= $begin_hour != '' ? $begin_hour : $time ?>">
+					<input type="time" name="begin_hour" id="begin_hour" value="<?= $form_data->begin_hour != '' ? $form_data->begin_hour : $time ?>">
 				</div>
 				<div>
 					<label for="description">Description</label>
-					<textarea name="description" id="description" value="<?= $description ?>"></textarea>
+					<textarea name="description" id="description" value="<?= $form_data->description ?>"></textarea>
 				</div>
 				<div>
 					<label for="place_nb">Nombre de places</label>
@@ -32,53 +32,41 @@
 			<h4>LIEUX</h4>
 			<section class="places">
 				<div>
-          <label class="label">Adresse</label>
-            <p class="control">
-              <input class="input is-medium search-adress" type="text" name="adress" id="adress" placeholder="Chercher une adresse" value="<?= $adress ?>" autocomplete="off">
-            </p>
-					<!-- <label for="adress">Adresse</label>
-					<input type="text" class="search-adress" name="adress" id="adress" value="<?= $adress ?>"> -->
+          			<label class="label">Adresse</label>
+            		<p class="control">
+              			<input class="input is-medium search-adress" type="text" name="adress" id="adress" placeholder="Chercher une adresse" value="<?= $form_data->adress ?>" autocomplete="off">
+            		</p>
 				</div>
-
-				<div class="result-list">
-				</div>
-				<!-- <div>
-					<label for="city">Ville</label>
-					<input type="text" name="city" id="city" value="<?= $city ?>">
-				</div>
-				<div>
-					<label for="zip_code">Code Postal</label>
-					<input type="text" name="zip_code" id="zip_code" value="<?= $zip_code ?>">
-				</div> -->
+				<div class="result-list"></div>
 			</section>
 
 			<h4>AUTRES</h4>
 			<section>
 				<div>
 					<label for="setup_display">Type d'écran</label>
-					<select name="setup_display" id="setup_display" value="<?= $setup_display ?>">
-						<option value="pc" <?= $setup_display == 'pc' ? 'selected' : '' ?> >Ordinateur</option>
-						<option value="tv" <?= $setup_display == 'tv' ? 'selected' : '' ?> >Télévision</option>
-						<option value="projecteur" <?= $setup_display == 'projecteur' ? 'selected' : '' ?> >Projecteur</option>
+					<select name="setup_display" id="setup_display" value="<?= $form_data->setup_display ?>">
+						<option value="pc" <?= $form_data->setup_display == 'pc' ? 'selected' : '' ?> >Ordinateur</option>
+						<option value="tv" <?= $form_data->setup_display == 'tv' ? 'selected' : '' ?> >Télévision</option>
+						<option value="projecteur" <?= $form_data->setup_display == 'projecteur' ? 'selected' : '' ?> >Projecteur</option>
 					</select>
 				</div>
 				<div>
 					<label for="setup_sound">Installation son</label>
-					<select name="setup_sound" id="setup_sound" value="<?= $setup_sound ?>">
-						<option value="natif" <?= $setup_sound == 'natif' ? 'selected' : '' ?> >Natif</option>
-						<option value="bq" <?= $setup_sound == 'bq' ? 'selected' : '' ?> >Enceinte Basse-Qualité</option>
-						<option value="hq" <?= $setup_sound == 'hq' ? 'selected' : '' ?> >Enceinte Haute-Qualité</option>
+					<select name="setup_sound" id="setup_sound" value="<?= $form_data->setup_sound ?>">
+						<option value="natif" <?= $form_data->setup_sound == 'natif' ? 'selected' : '' ?> >Natif</option>
+						<option value="bq" <?= $form_data->setup_sound == 'bq' ? 'selected' : '' ?> >Enceinte Basse-Qualité</option>
+						<option value="hq" <?= $form_data->setup_sound == 'hq' ? 'selected' : '' ?> >Enceinte Haute-Qualité</option>
 					</select>
 				</div>
 				<div>
 					<label for="supp_info">Informations complémentaires</label>
-					<textarea name="supp_info" id="supp_info" value="<?= $supp_info ?>" placeholder="Je n'accepte pas les enfants - Apportez des sushis - Venez déguisés"></textarea>
+					<textarea name="supp_info" id="supp_info" value="<?= $form_data->supp_info ?>" placeholder="Je n'accepte pas les enfants - Apportez des sushis - Venez déguisés"></textarea>
 				</div>
 			</section>
 			<section>
 				<div>
 					<label for="movie_name">Nom du film</label>
-					<input type="text" class="search-movie" name="movie_name" id="movie_name" value="<?= $movie_name ?>">
+					<input type="text" class="search-movie" name="movie_name" id="movie_name" value="<?= $form_data->movie_name ?>">
 				</div>
 			</section>
 
