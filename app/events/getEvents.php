af6779059df9882872820dc90ddcbcaf5b2ef5d7
mickaelzhang <mickael.zhg@gmail.com>
2016-04-14 20:43:49 +0200

    Resolved - Send attend AJAX

--- app/events/getEvents.php
+++ app/events/getEvents.php
@@ -215,7 +215,7 @@ class getEvents extends Database {
     public function addRequest($event_id,$user_id) {
       $query = "INSERT INTO attend (event_id,user_id)
                 VALUES ($event_id,$user_id)";
-      $prepare = $pdo->prepareQuery($query);
+      $prepare = $this->prepareQuery($query);
       return $prepare;
     }
 
@@ -224,7 +224,7 @@ class getEvents extends Database {
       $query = "UPDATE events
             SET pending_request = pending_request + 1
             WHERE event_id = $event_id";
-      $prepare = $pdo->prepareQuery($query);
+      $prepare = $this->prepareQuery($query);
       return $prepare;
     }
 }
--- controllers/ajax/send-attend.php
+++ controllers/ajax/send-attend.php
@@ -7,9 +7,9 @@ $event_id = $_GET['event_id'];
 $user_id = $_SESSION['user_id'];
 
 // Search if its already done or not
-$result = getAttendInfo ($user_id, $event_id)
+$result = $update->getAttendInfo ($user_id, $event_id);
 
-if (empty($result)) {
+if ($result == NULL) {
 	$update->addRequest($event_id,$user_id);
 	$update->addPendingRequest($event_id);
 }
