10f14f9e539f5053a6c35cd4d102ff31bc58de78
Nans Thomas <nans.thomas@hetic.net>
2016-04-15 15:12:22 +0200

    Push some change 😪

--- views/partials/html-top.php
+++ views/partials/html-top.php
@@ -5,6 +5,7 @@
 	<title><?= $title ?></title>
 	<link rel="stylesheet" href="<?= URL ?>src/css/bulma.min.css">
 	<link rel="stylesheet" href="<?= URL ?>src/css/style.min.css">
+  <link rel="icon" type="image/png" href="<?= URL ?>favicon.png" /> 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
 <body>
